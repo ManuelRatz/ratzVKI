dfa767cf0a614d30f8c751fad6af526ac1931827
ManuelRatz <manuel.ratz@vki.ac.be>
2020-09-30 16:10:48 +0200

    updated files only using the right contact angle

--- VKI_RM-master_Anna/Own_model/own_derivation_calc.py
+++ VKI_RM-master_Anna/Own_model/own_derivation_calc.py
@@ -8,60 +8,65 @@ from scipy.integrate import odeint
 
 plt.rc('font', size=15)          # controls default text sizes
 plt.rc('axes', titlesize=15)     # fontsize of the axes title
-plt.rc('axes', labelsize=20)    # fontsize of the x and y labels
+plt.rc('axes', labelsize=20)     # fontsize of the x and y labels
 plt.rc('xtick', labelsize=15)    # fontsize of the tick labels
 plt.rc('ytick', labelsize=15)    # fontsize of the tick labels
 plt.rc('legend', fontsize=15)    # legend fontsize
-plt.rc('figure', titlesize=20) # fontsize of the figure title
-plt.rc('text', usetex=True)
-plt.rc('font', family='serif')
-plt.rc('axes', grid=True)
-plt.rc('savefig', dpi = 100)
+plt.rc('figure', titlesize=20)   # fontsize of the figure title
+plt.rc('text', usetex=True)      # use latex for the text
+plt.rc('font', family='serif')   # serif as text font
+plt.rc('axes', grid=True)        # enable the grid
+plt.rc('savefig', dpi = 100)     # set the dpi for saving figures
 
-#set the constants
-g      = 9.81;         # Gravity (m/s2)
-Patm   = 1e5  # atmospheric pressure, Pa
-rhoL   = 1000;         #Fluid Density (kg/m3)
+# set the constants (this is for water)
+g      = 9.81;          # Gravity (m/s2)
+Patm   = 1e5            # atmospheric pressure, Pa
+rhoL   = 1000;          # Fluid Density (kg/m3)
 mu     = 8.90*10**(-4); # Fuid dynamic viscosity (Pa*s)
-sigma  = 0.07197;      # surface tension (N/m)
-r      = 0.0025       # radius of the tube (m)
-delta  = 2* r   # channel width (m)
-l      = 100*r   # depth of the channel
+sigma  = 0.07197;       # surface tension (N/m)
+r      = 0.0025         # radius of the tube (m)
+delta  = 2* r           # channel width (m)
+l      = 100*r          # depth of the channel
 
-#Test cases used for evaluation
+# Test cases used for evaluation
 pressure_case = np.array([1000, 1250, 1500])
-#here the pressure steps as measured by the sensor are given
+# here the pressure steps as measured by the sensor are given
 pressure_step = np.array([662.6799, 823.1328, 990.89])
 
+# set up the for loop
 for i in range(0, 3):
-    #Update the user about the status
+    # Update the user about the status
     print('Calculating pressure %d of %d' %((i+1), len(pressure_case)))
-    #load the data
+    # load the data
     FOLDER = '..' + os.sep + 'experimental_data' + os.sep + '%d_pascal' %pressure_case[i] + os.sep
     lca_load = np.genfromtxt(FOLDER + 'lca_%d.txt' %pressure_case[i])
     rca_load = np.genfromtxt(FOLDER + 'rca_%d.txt' %pressure_case[i])
     h_cl_l_load = np.genfromtxt(FOLDER + 'cl_l_%d.txt' %pressure_case[i])
     h_cl_r_load = np.genfromtxt(FOLDER + 'cl_r_%d.txt' %pressure_case[i])
     pressure_load = np.genfromtxt(FOLDER + 'pressure_%d.txt' %pressure_case[i])
+    h_avg_load = np.genfromtxt(FOLDER + 'avg_height_%d.txt' %pressure_case[i])
     
-    #calculate the averages
-    ca = (lca_load + rca_load) / 2
+    # calculate the averages
+    ca = rca_load
     h_cl = (h_cl_l_load + h_cl_r_load) / 2
     
-    #shift the data
+    # shift the data
     shift_idx = np.argmax(ca > 0)
     ca = ca[shift_idx:]
     h_cl = h_cl[shift_idx:]
+    h_avg_load = h_avg_load[shift_idx:]
     pressure_load= pressure_load[shift_idx:len(ca)+shift_idx]
     
-    #smooth the data 
-    ca_smoothed = savgol_filter(ca, 35, 3, axis = 0)
+    # smooth the data 
+    ca_smoothed =  ca# savgol_filter(ca, 9, 3, axis = 0)
     h_cl_smoothed = savgol_filter(h_cl, 105, 3, axis = 0)
+    h_smoothed = savgol_filter(h_avg_load, 9, 2, axis = 0)
     pressure_smoothed = savgol_filter(pressure_load, 151, 3, axis = 0)
     
     #set up the timesteps
     t = np.arange(0, len(ca)/500, 0.002)
     
+    # here we set up the 4 different pressure signals
     step_pressure = np.zeros(len(pressure_smoothed)) + pressure_step[i]
     advanced_step_pressure = np.copy(pressure_smoothed)
     idx = np.argmax(pressure_smoothed > pressure_step[i])
@@ -110,8 +115,8 @@ for i in range(0, 3):
     """
     
     #set the inital values
-    h0 = h_cl_smoothed[0]
-    velocity = np.gradient(h_cl_smoothed)
+    h0 = h_smoothed[0]
+    velocity = np.gradient(h_smoothed)
     dh0 = velocity[0]/(0.002)
     X0 = np.array([dh0, h0])
     

6d8adae6b5419d62113f0cafa443bfb56472b197
ManuelRatz <70625199+ManuelRatz@users.noreply.github.com>
2020-11-17 15:58:09 +0100

    Update PressureLVM.py
    Deleted unnecessary code

--- PIV_Campaign_Processing/PressureLVM.py
+++ PIV_Campaign_Processing/PressureLVM.py
@@ -18,8 +18,7 @@ from matplotlib import pyplot as plt
 
 
 #%% inputs
-# folder = 'C:/Users/fiorini/Documents/DomenicoFIORINI/VKI_PHD/PROJECT/CURRENT_EXPERIMENTAL_CAMPAIGNS/20201022_testPressureEquivalence/test6'
-folder = ''
+folder = 'C:\PIV_Campaign\Rise\h1\p1500\R_h1_f1000_2_p15' + os.sep
 #%% functions....
 
 def read_lvm(path):
@@ -41,10 +40,10 @@ def read_lvm(path):
 
 #%% paths and folders (normally they are in agree with the codes in the same folder)
 
-path_valve_p = folder +'R_h1_f1200_1_p12-utube-valve_p_001.lvm'
-path_pressure= folder +'R_h1_f1200_1_p12-utube-pressure_001.lvm'
-path_pressure_n= folder +'R_h1_f1200_1_p12-utube-pressure_n_001.lvm'
-path_valve_n = folder +'R_h1_f1200_1_p12-utube-valve_n_001.lvm'
+path_valve_p = folder +'R_h1_f1000_2_p15-utube-valve_p_001.lvm'
+path_pressure= folder +'R_h1_f1000_2_p15-utube-pressure_001.lvm'
+path_pressure_n= folder +'R_h1_f1000_2_p15-utube-pressure_n_001.lvm'
+path_valve_n = folder +'R_h1_f1000_2_p15-utube-valve_n_001.lvm'
 
 
 
@@ -53,20 +52,12 @@ path_valve_n = folder +'R_h1_f1200_1_p12-utube-valve_n_001.lvm'
 [time, voltages]  = read_lvm(path_pressure)
 p_pressure        = [voltage*208.73543056621196-11.817265775905382 for voltage in voltages]
 
-# [time, voltages]  = read_lvm(path_pressure_n)
-# p_pressure_n      = [voltage*206.32187068310276-11.767651474219058 for voltage in voltages]
-
 # # valves
 [t_valve_p, v_valve_p]   = read_lvm(path_valve_p)
 g_valve_p        = np.gradient(v_valve_p,t_valve_p)
-
-# [t_valve_n, v_valve_n]   = read_lvm(path_valve_n)
-# g_valve_n        = np.gradient(v_valve_n,t_valve_n)
 
 f_acq_labview = (len(t_valve_p)-1)/t_valve_p[-1]
 
 start = np.argmax(g_valve_p > 500)
 t = np.arange(0, 4, 0.001)
 plt.plot(t,p_pressure[start:start+4000])
-# plt.plot(v_valve_p*200)
-# plt.plot(g_valve_p)

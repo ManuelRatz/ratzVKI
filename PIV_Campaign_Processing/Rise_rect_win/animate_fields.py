655ff266b2ef000aaa94a29a4fc6b40fc52f6bb7
ManuelRatz <70625199+ManuelRatz@users.noreply.github.com>
2020-11-17 13:13:57 +0100

    Created file to create and animate contour and quiverplots.

---
+++ PIV_Campaign_Processing/Rise_rect_win/animate_fields.py
@@ -0,0 +1,78 @@
+"""
+Created on Fri Nov 13 09:40:18 2020
+
+@author: Manuel
+@description: Code to animate velocity fields with quivers and contours
+"""
+
+import post_processing_functions as ppf # for reshaping the arrays
+import matplotlib.pyplot as plt         # for plotting
+import imageio                          # for rendering the gif
+import numpy as np                      # for array operations
+import os                               # for file paths
+import shutil                           # for removing folders
+
+# set the plot parameters in case they were changed in rc.params
+ppf.set_plot_parameters()
+
+# set the input folder
+Fol_In = 'C:\\Users\manue\Desktop\Results_16_64_R_h1_f1200_1_p15'
+# set the folder in which to store gif images (will be deleted in the end)
+Gif_Images = 'C:\\Users\manue\Desktop\Gif_Images'
+if not os.path.exists(Gif_Images):
+    os.makedirs(Gif_Images)
+
+# get the amount of columns in the files
+nx = ppf.get_column_amount(Fol_In)
+
+# set the gif name and an empty list for the images to append into
+GIFNAME = Gif_Images + os.sep + '..' +os.sep + 'rise_inversion.gif'
+IMAGES = []
+# give the starting frame and the amount of images to process
+frame0 = 0
+n_t = 70
+# plot every IMG_STP_SZ'th image
+IMG_STP_SZ =15
+
+# set a custom colormap
+custom_map = ppf.custom_div_cmap(100, mincol='indigo', midcol='darkcyan' ,maxcol='yellow')
+
+# loop over the images
+for i in range(0, n_t):
+    # update user
+    print('Image %d of %d' %((i+1, n_t)))
+    # calculate the loadindex (we need this multiple times)
+    load_idx = frame0+IMG_STP_SZ*i
+    # load the data into arrays
+    x, y, u, v = ppf.load_txt(Fol_In, load_idx, nx)
+    # create the figure
+    fig, ax = plt.subplots(figsize = (5,10))
+    # create the contourplot using pcolormesh
+    cs = plt.pcolormesh(x,y,v, vmin=-10, vmax=16, cmap = custom_map)
+    # set the correct aspect ratio
+    ax.set_aspect('equal')
+    # get the colorbar
+    clb = fig.colorbar(cs)
+    # set the colorbarticks
+    clb.set_ticks(np.arange(-10, 17, 2))
+    # set the colorbar title
+    clb.ax.set_title('Velocity\n [px/frame] \n \n')
+    # set a stepsize in the x and y direction to avoid overcrowding the quiver plot
+    STEPY= 2
+    STEPX = 1
+    # plot the quivers
+    ax.quiver(x[::STEPY, ::STEPX], y[::STEPY, ::STEPX], u[::STEPY, ::STEPX], v[::STEPY, ::STEPX],\
+            color='k', scale =30, width=0.005,headwidth=4, headaxislength = 6)
+    # set the title
+    ax.set_title('Image %06d' % load_idx)
+    # set the output name
+    Name_Out = Gif_Images + os.sep + '%06d.jpg' % (load_idx)
+    # save the figure
+    fig.savefig(Name_Out, dpi = 75)
+    # append into the imagelist
+    IMAGES.append(imageio.imread(Name_Out))
+    # close the figure to avoid overcrowding the plot window
+    plt.close(fig)
+imageio.mimsave(GIFNAME, IMAGES, duration = 0.1)
+
+shutil.rmtree(Gif_Images)

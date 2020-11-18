39090db2662bea66099445305630d5f41aaa89bd
ManuelRatz <70625199+ManuelRatz@users.noreply.github.com>
2020-11-18 14:08:44 +0100

    Update 2_OpenPiv_windef_client_rect.py
    Ran multiple test runs for the calculation time

--- PIV_Campaign_Processing/Rise_rect_win/2_OpenPiv_windef_client_rect.py
+++ PIV_Campaign_Processing/Rise_rect_win/2_OpenPiv_windef_client_rect.py
@@ -19,7 +19,7 @@ class Settings(object):
 settings = Settings()
 
 'Data related settings'
-settings.save_folder_suffix = 'R_h1_f1200_1_p15'
+settings.save_folder_suffix = 'R_h2_f1200_1_p15'
 # Folder with the images to process
 settings.filepath_images = 'C:\PIV_Processed\Images_Preprocessed' + os.sep + settings.save_folder_suffix + os.sep
 # Folder for the outputs
@@ -48,15 +48,16 @@ settings.correlation_method = 'circular'  # 'circular' or 'linear'
 settings.iterations = 3 # select the number of PIV passes
 
 """
+
 Here we set the window sizes. This code uses rectangular windows, if this is 
 not desired, simply put the same values for window_height and window_width as
 well as overlap_height and overlap_width
 """
 # base 2
-settings.window_height = ( 128, 64, 48)
-settings.overlap_height = ( 64, 32, 24)
-settings.window_width = (32, 16, 12, 16)
-settings.overlap_width = (16, 8, 6, 8) 
+settings.window_height = (256, 128, 64)
+settings.overlap_height = (128, 64, 32)
+settings.window_width = (16, 16, 16)
+settings.overlap_width = (8, 8, 8) 
 # # base 3
 # settings.window_height = (192, 96, 48, 24, 12)
 # settings.overlap_height = (96, 48, 24, 12, 6) # 50%
@@ -67,13 +68,13 @@ settings.overlap_width = (16, 8, 6, 8)
 settings.extract_sig2noise = True  # 'True' or 'False' (only for the last pass)
 settings.sig2noise_method = 'peak2peak'
 settings.sig2noise_mask = 3
-settings.do_sig2noise_validation = False # This is time consuming
+settings.do_sig2noise_validation = True # This is time consuming
 settings.sig2noise_threshold = 1.3
 
 # validation
 settings.validation_first_pass = True
-settings.MinMax_U_disp = (-10, 10)
-settings.MinMax_V_disp = (-100, 100)
+settings.MinMax_U_disp = (-3, 3)
+settings.MinMax_V_disp = (-20, 20)
 settings.std_threshold = 70 # threshold of the std validation
 settings.median_threshold = 50  # threshold of the median validation
 settings.median_size = 1 
@@ -89,27 +90,27 @@ settings.smoothn_p=0.01 # This is a smoothing parameter
 # cosmetics
 settings.scaling_factor = 1  # scaling factor pixel/meter
 settings.dt = 1  # time between to frames (in seconds)
-settings.save_plot = True
+settings.save_plot = False
 settings.show_plot = False
 settings.scale_plot = 200 # select a value to scale the quiver plot of the vectorfield
 
-settings.beginning_index = 279
+settings.beginning_index = 335
 
 # run the script with these settings
 
-height = np.array([[256,128,96],[256,128,64],[128, 64, 48],[256,128,64]])
-overlap_height = np.array([[128,64,48],[128,64,32],[64, 32, 24],[128,64,32]])
-width = np.array([[64,32,24],[64,32,16],[32, 16, 12],[16,16,16]])
-overlap_width= np.array([[32,16,12],[32,16,8],[16, 8, 6],[8,8,8]])
+# height = np.array([[256,128,96],[256,128,64],[128, 64, 48],[256,128,64]])
+# overlap_height = np.array([[128,64,48],[128,64,32],[64, 32, 24],[128,64,32]])
+# width = np.array([[64,32,24],[64,32,16],[32, 16, 12],[16,16,16]])
+# overlap_width= np.array([[32,16,12],[32,16,8],[16, 8, 6],[8,8,8]])
 import time
-for i in range(1,2):
-    start = time.time()
-    settings.window_height = height[i]
-    settings.overlap_height = overlap_height[i]
-    settings.window_width = width[i]
-    settings.overlap_width = overlap_width[i]
-    piv(settings)
-    print(time.time()-start)
+# for i in range(1,2):
+start = time.time()
+    # settings.window_height = height[i]
+    # settings.overlap_height = overlap_height[i]
+    # settings.window_width = width[i]
+    # settings.overlap_width = overlap_width[i]
+piv(settings)
+print(time.time()-start)
 
 
 

bff1b1d034fc51425d6d1b8ab709677e419929e1
ManuelRatz <70625199+ManuelRatz@users.noreply.github.com>
2020-11-25 08:48:24 +0100

    Update client_fall.py
    Added description

--- PIV_Campaign_Processing/Fall_processing/client_fall.py
+++ PIV_Campaign_Processing/Fall_processing/client_fall.py
@@ -2,17 +2,49 @@
 """
 Created on Wed Sep 18 16:42:13 2019
 
-@author: Theo
+@author: Manuel
+@description:
+##############################################################################
+
+ IMPORTANT READ ME           IMPORTANT READ ME              IMPORTANT READ ME
+                               
+##############################################################################
+
+This code contains the power to process all the Falls of the piv campaign. To
+use it simply clone the github repository and you will have all the files. In
+the end there is a loop. To process all the images simply let the file run as is.
+
+This will likely result in an error. You have to restart the kernel after a few
+runs depending on the power of your computer. To be sure restart after every run.
+
+The idea is to run two different settings:
+    One with rectangles (Do for ALL images), the window sizes for this are 
+        settings.window_height = (64, 32, 24)
+        settings.overlap_height = (32, 16, 12)
+        settings.window_width = (64, 32, 24)
+        settings.overlap_width = (32, 16, 12) 
+    One with squares (Do for the 1000 and 1200 Hz runs), the window sizes for this are
+        settings.window_height = (128, 64, 32)
+        settings.overlap_height = (64, 32, 16)
+        settings.window_width = (128, 64, 32)
+        settings.overlap_width = (64, 32, 16)
+
+The raw images are stored in a .rar file under the following link (YET TO BE DONE, 24.11.)
+    https://osf.io/vp3cq/
+Simply download them and store them in the given data path or change according to your needs.
+
+Currently we are not processing two of the rises:
+    - R_h1_f750_p10 - because the displacement is too high
+    - R_h1_f1000_1_p14 - because two images have been replaced by dummys which
+                         will mess up the ROI prediction
+                         
+Except for the settings in the loop, no other ones should be changed.
 """
 
-# add two directories that include the new files
-# note that we need to import openpiv in a separate, original namespace
-# so we can use everything from openpiv as openpiv.filters and whatever is 
-# going to replace it will be just filteers (for example)
 
 import os
 import numpy as np
-from windef_rect_moving_roi_fall import piv
+from windef_rect_fall import piv
 
 class Settings(object):
     pass  
@@ -22,18 +54,13 @@ settings = Settings()
 
 # Folder for the outputs
 settings.save_path = 'C:\PIV_Processed\Images_Processed'
-  
-'Region of interest'
-# (50,300,50,300) #Region of interest: (xmin,xmax,ymin,ymax) or 'full' for full image
-settings.ROI = np.array([0,1280,0,500]) # The first number is the position of the interface measured from the bottom of the image
-# settings.ROI = 'full'
+settings.ROI = np.array([0,1280,0,500])
+
 
 'Image preprocessing'
 settings.dynamic_masking_method = 'None'
 settings.dynamic_masking_threshold = 0.005
 settings.dynamic_masking_filter_size = 7 
-
-
 
 # windows and displacement calculation
 settings.interpolation_order = 3
@@ -45,11 +72,6 @@ settings.window_height = (64, 32, 24)
 settings.overlap_height = (32, 16, 12)
 settings.window_width = (64, 32, 24)
 settings.overlap_width = (32, 16, 12) 
-# base 3
-# settings.window_height = (96, 48, 24, 12)
-# settings.overlap_height = (48, 24, 12, 6) # 50%
-# settings.window_width = (96, 48, 24, 12)
-# settings.overlap_width = (48, 24, 12, 6) # 50%
 
 # sig2noise
 settings.extract_sig2noise = True  # 'True' or 'False' (only for the last pass)
@@ -92,13 +114,11 @@ for i in range(2, 3):
     # Format and Image Sequence
     settings.frame_pattern_a = observation_periods[i, 0] + '.*.tif'
     settings.frame_pattern_b = None  
-    settings.fall_start = int(observation_periods[i, 1])
-    settings.roi_shift_start = int(observation_periods[i, 2])
-    settings.plot_roi = True
-    settings.amount = None
+    settings.fall_start = int(observation_periods[i, 1]) # index after which the liquid is moving
+    settings.roi_shift_start = int(observation_periods[i, 2]) # index after which to shift the ROI
     settings.process_fall = True
     settings.process_roi_shift = True
-    settings.run = 4
+    settings.run = 1
     piv(settings)
     
 

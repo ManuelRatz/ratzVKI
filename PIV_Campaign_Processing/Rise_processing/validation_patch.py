0a5f685700f70fc153842d39188110386401198f
ManuelRatz <70625199+ManuelRatz@users.noreply.github.com>
2020-12-02 13:07:47 +0100

    Updated sig2noise to have a maximum of 50

--- PIV_Campaign_Processing/Rise_processing/validation_patch.py
+++ PIV_Campaign_Processing/Rise_processing/validation_patch.py
@@ -122,7 +122,7 @@ def global_std( u, v, std_threshold = 3 ):
     
     return u, v, mask
 
-def sig2noise_val( u, v, sig2noise, w=None, threshold_low = 1.3, threshold_high = 15):
+def sig2noise_val( u, v, sig2noise, w=None, threshold_low = 1.3, threshold_high = 50):
     """Eliminate spurious vectors from cross-correlation signal to noise ratio.
     
     Replace spurious vectors with zero if signal to noise ratio
@@ -166,7 +166,6 @@ def sig2noise_val( u, v, sig2noise, w=None, threshold_low = 1.3, threshold_high 
     R. D. Keane and R. J. Adrian, Measurement Science & Technology,1990, 1, 1202-1215.
     
     """
-    threshold_high = 50
     ind = (sig2noise < threshold_low) + (sig2noise > threshold_high)
     u[ind] = np.nan
     v[ind] = np.nan

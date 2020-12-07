025e9c2fb167815d694a316ad9e6b6c5c7848d57
ManuelRatz <70625199+ManuelRatz@users.noreply.github.com>
2020-12-07 17:09:34 +0100

    Added Histogram plotting, invalid filtering and highpass filtered quiver

--- PIV_Campaign_Processing/Rise_processing/slide_animations_rise.py
+++ PIV_Campaign_Processing/Rise_processing/slide_animations_rise.py
@@ -6,33 +6,36 @@ Created on Fri Nov 20 11:46:08 2020
 @description: Create animations for the slide. These are animations for
     the R_h2_f1200_1_p13 
 """
-
+import sys
+sys.path.append('C:\\Users\manue\Documents\GitHub\\ratzVKI\PIV_Campaign_Processing')
 import os                               # for file paths
 import numpy as np                      # for calculations
 import matplotlib.pyplot as plt         # for plotting
 import post_processing_functions as ppf # for some postprocessing stuff
 import shutil                           # for deleting the folder with the gif images
 import imageio                          # for animations
-
+import warnings
+warnings.filterwarnings("ignore")
 # set the plot parameters
-ppf.set_plot_parameters()
+ppf.set_plot_parameters(20, 15, 20)
 
 # give the input folder for the data
-Fol_In = 'C:\PIV_Processed\Images_Processed\Results_R_h2_f1200_1_p13_64_16'
+Fol_In = 'C:\PIV_Processed\Images_Processed\Rise_64_16_peak2RMS\Results_R_h2_f1200_1_p13_64_16'
 # give the input folder of the raw images (this is required to get the image width and frequency)
-Fol_Raw = 'C:\PIV_Processed\Images_Preprocessed\R_h2_f1200_1_p13'
+Fol_Raw = ppf.get_raw_folder(Fol_In)
 
 # set the constants
-IMG_WIDTH = ppf.get_img_width(Fol_Raw) # image width in pixels
-SCALE = IMG_WIDTH/5 # scaling factor in px/mm
-DT = 1/ppf.get_frequency(Fol_Raw) # time between images
-FACTOR = 1/(SCALE*DT) # conversion factor to go from px/frame to mm/s
+Height, Width = ppf.get_img_shape(Fol_Raw) # image width in pixels
+Scale = Width/5 # scaling factor in px/mm
+Dt = 1/ppf.get_frequency(Fol_Raw) # time between images
+Factor = 1/(Scale*Dt) # conversion factor to go from px/frame to mm/s
 NX = ppf.get_column_amount(Fol_In) # get the number of columns
 # set frame0, the image step size and how many images to process
-FRAME0 = 391 # starting index of the run
-STP_SZ = 10 # step size in time
-SECONDS = 2 # how many seconds to observe the whole thing
-N_T = int((SECONDS/DT)/STP_SZ)
+Frame0 = 354 # starting index of the run
+Stp_T = 2 # step size in time
+Seconds = 2 # how many seconds to observe the whole thing
+# N_T = int((Seconds/Dt)/Stp_T)
+N_T = 250
 
 # set up empty listss to append into and the names of the gifs
 IMAGES_ROI = []
@@ -40,19 +43,22 @@ IMAGES_H = []
 IMAGES_CONT = []
 IMAGES_PROF = []
 IMAGES_FLUX = []
-GIF_ROI = 'changing_roi.gif'
-GIF_H = 'h.gif'
-GIF_QUIV = 'contour.gif'
-GIF_PROF = 'profiles.gif'
-GIF_FLUX = 'flux.gif'
-
+IMAGES_HIGHPASS = []
+IMAGES_HIST = []
+GIF_ROI = 'changing_roi_%d.gif' %(N_T)
+GIF_H = 'h_%d.gif'%(N_T)
+GIF_QUIV = 'contour_%d.gif'%(N_T)
+GIF_PROF = 'profiles_%d.gif'%(N_T)
+GIF_FLUX = 'flux_%d.gif'%(N_T)
+GIF_HIGHPASS = 'highpass_filtered_%d.gif'%(N_T)
+GIF_HIST = 'histogram_%d.gif'%(N_T)
 # create a folder to store the images
 Fol_Img = ppf.create_folder('images')
 
 # load the data of the predicted height
 h = ppf.load_h(Fol_In)
 # set up a time array for plotting
-t = np.linspace(0,SECONDS,int(SECONDS/DT)+1)[::STP_SZ]
+t = np.linspace(0,Seconds,int(Seconds/Dt)+1)[::Stp_T]
 
 # set a custom colormap
 custom_map = ppf.custom_div_cmap(100, mincol='indigo', midcol='darkcyan' ,maxcol='yellow')
@@ -63,28 +69,66 @@ PLOT_HT = True
 PLOT_QUIV = True
 PLOT_PROF = True
 PLOT_FLUX = True
+PLOT_HIGHPASS = True
+PLOT_HIST = True
 
-for i in range(0,N_T):
-    print('Image %d of %d' %((i+1,N_T)))
+for II in range(0,N_T):
+    print('Image %d of %d' %((II+1,N_T)))
     # calculate the loading index and load the data
-    LOAD_IDX = FRAME0 + STP_SZ*i
-    x, y, u, v, sig2noise, valid = ppf.load_txt(Fol_In, LOAD_IDX, NX)
+    LOAD_IDX = Frame0 + Stp_T*II
+    x, y, u, v, ratio, mask = ppf.load_txt(Fol_In, LOAD_IDX, NX)
+    if PLOT_HIST == True:
+        fig, ax = plt.subplots(figsize = (8, 5))
+        ax.hist(ratio.ravel(), bins = 100, density = True)
+        ax.set_xlim(0, 30)
+        ax.set_ylim(0, 0.3)
+        ax.grid(b = True)
+        ax.set_xlabel('Signal to noise ratio')
+        ax.set_ylabel('Probability')
+        ax.plot((6.5, 6.5), (0, 0.3), c = 'r', label = 'Treshold')
+        ax.legend(loc = 'upper right')
+        Name_Out = Fol_Img+os.sep+'s2n%06d.png'%LOAD_IDX  
+        fig.savefig(Name_Out, dpi = 65)
+        plt.close(fig)
+        IMAGES_HIST.append(imageio.imread(Name_Out)) # append into list
+    
+    # filter out the invalid rows 
+    mask_nan = np.isfinite(ratio)
+    mask = ~mask.astype('bool')
+    valid = mask*mask_nan
+    N_Rows = u.shape[0]
+    for i in range(0,u.shape[0]):
+        index = u.shape[0]-i-1
+        if np.sum(valid[index,:])/valid.shape[0] < 0.5:
+            v = v[index+2:]
+            u = u[index+2:]
+            x = x[index+2:]
+            y = y[index+2:]
+            ratio = ratio[index+2:]
+            valid = valid[index+2:]
+            break
+    invalid = ~valid.astype('bool')
+
     # convert to mm/s
-    u = u*FACTOR
-    v = v*FACTOR
+    u = u*Factor
+    v = v*Factor
+    # pad the data using the no slip boundary condition
+    x_pad, y_pad, u_pad, v_pad = ppf.pad(x, y, u, v, Width)   
+    x_pco, y_pco = ppf.shift_grid(x, y)
+    u_hp, v_hp = ppf.high_pass(u, v, sigma = 3, truncate = 3)
+    img = ppf.load_raw_image(Fol_Raw, LOAD_IDX)
+    
     # start with the animation of the changing ROI
     if PLOT_ROI == True:
-        name = Fol_Raw + os.sep + 'R_h2_f1200_1_p13.%06d.tif' %(LOAD_IDX+1) # because we want frame_b
-        img = imageio.imread(name)
         # create the figure
         fig, ax = plt.subplots(figsize = (2.5,8))
-        ax.imshow(img, cmap=plt.cm.gray) # show the image
-        ax.set_yticks(np.arange(170,1300,220)) # set custom y ticks (not automatized)
-        ax.set_yticklabels((20,16,12,8,4,0), fontsize=15) # set custom labels (not automatized)
-        ax.set_xticks((0,55,110,165,220,274)) # set custom x ticks (not automatized)
-        ax.set_xticklabels(np.arange(0,6,1), fontsize=15) # set custom y ticks (not automatized)
-        ax.set_xlabel('$x$[mm]', fontsize=20) # set x label
-        ax.set_ylabel('$y$[mm]', fontsize=20) # set y label
+        ax.imshow(np.flip(img, axis = 0), cmap=plt.cm.gray) # show the image
+        ax.set_yticks(np.arange(Height-20*Scale-1,img.shape[0],4*Scale)) # set custom y ticks
+        ax.set_yticklabels(np.linspace(0,20,6,dtype=int)[::-1], fontsize=15) # set custom y ticklabels
+        ax.set_xticks(np.linspace(0,Width-1, 6)) # set custom x ticks 
+        ax.set_xticklabels(np.arange(0,6,1), fontsize=15) # set custom x ticklabels
+        ax.set_xlabel('$x$[mm]') # set x label
+        ax.set_ylabel('$y$[mm]') # set y label
         fig.tight_layout(pad=1.1) # crop edges of the figure to save space
         # plot a horizontal line of the predicted interface in case it is visible
         if h[LOAD_IDX] > 0:
@@ -99,17 +143,17 @@ for i in range(0,N_T):
         # now the plot for height over time, the principle is the same, so not everything is commented
         fig, ax = plt.subplots(figsize=(8,5))
         # crop the height before frame0
-        h_dum = h[FRAME0-1:]
+        h_dum = h[Frame0-1:]
         # shift it to only get the steps
-        h_dum = h_dum[::STP_SZ]
+        h_dum = h_dum[::Stp_T]
         # plot the height, convert it to mm and shift to have the beginning height at the top of the image
-        plot1 = ax.plot(t[:i+1],(-h_dum[:i+1]+1271)/SCALE, c='r', label = 'Interface\nHeight')
-        ax.set_title('$t$ = %03d [ms]' %(t[i]*1000))
-        ax.scatter(t[i],(-h_dum[i]+1271)/55, c='r', marker='x', s=(300./fig.dpi)**2)
+        ax.plot(t[:II+1],(-h_dum[:II+1]+Height)/Scale, c='r', label = 'Interface\nHeight')
+        ax.set_title('$t$ = %03d [ms]' %(t[II]*1000))
+        ax.scatter(t[II],(-h_dum[II]+Height)/Scale, c='r', marker='x', s=(300./fig.dpi)**2)
         ax.set_ylim(0,30)
         ax.set_xlim(0,1)
-        ax.set_xlabel('$t$[s]', fontsize = 20)
-        ax.set_ylabel('$h$[mm]', fontsize = 20)
+        ax.set_xlabel('$t$[s]')
+        ax.set_ylabel('$h$[mm]')
         ax.grid(b=True)
         ax.set_xticks(np.arange(0, 2.1 ,0.2))
         ax.set_yticks(np.arange(0, 35, 5))
@@ -120,57 +164,26 @@ for i in range(0,N_T):
         plt.close(fig)
         IMAGES_H.append(imageio.imread(Name_Out))
     
-    # plot the contour and the quiver, the principle is the same, so not everything is commented
-    if PLOT_QUIV == True:
-        fig, ax = plt.subplots(figsize = (4, 8))
-        cs = plt.pcolormesh(x,y,v, vmin=-200, vmax=200, cmap = custom_map) # create the contourplot using pcolormesh
-        ax.set_aspect('equal') # set the correct aspect ratio
-        clb = fig.colorbar(cs, pad = 0.2) # get the colorbar
-        clb.set_ticks(np.arange(-200, 201, 40)) # set the colorbarticks
-        clb.ax.set_title('Velocity \n [mm/s]', pad=15) # set the colorbar title
-        STEPY= 2
-        STEPX = 1
-        plt.quiver(x[len(x)%2::STEPY, ::STEPX], y[len(x)%2::STEPY, ::STEPX], u[len(x)%2::STEPY, ::STEPX], v[len(x)%2::STEPY, ::STEPX],\
-                   color='k', scale=600, width=0.005,headwidth=4, headaxislength = 6)
-        ax.set_ylim(0,1271)
-        ax.set_yticks(np.arange(0,1300,220))
-        ax.set_yticklabels((0,4,8,12,16,20), fontsize=15)
-        ax.set_xlim(0,5)
-        ax.set_xticks(np.linspace(0,275,6))
-        ax.set_xticklabels(np.arange(0,6,1), fontsize=15)
-        ax.set_xlabel('$x$[mm]', fontsize=20)
-        ax.set_ylabel('$y$[mm]', fontsize=20)
-        fig.tight_layout(pad=0.5)
-        Name_Out = Fol_Img+os.sep+'contour%06d.png'%LOAD_IDX
-        fig.savefig(Name_Out, dpi=65)
-        plt.close(fig)
-        IMAGES_CONT.append(imageio.imread(Name_Out))
-    
-    # pad the data using the no slip boundary condition
-    pad_0 = np.zeros((x.shape[0],1))
-    pad_x_max = np.ones((x.shape[0],1))*IMG_WIDTH
-    x = np.hstack((pad_0, x, pad_x_max))
-    v = np.hstack((pad_0, v, pad_0))
-    
+
     # plot the velocity profiles, the principle is the same, so not everything is commented
     if PLOT_PROF == True:
         fig, ax = plt.subplots(figsize=(8,5))
         # initialize array with values every 25% of the ROI
-        y_IND = np.array([int(len(x)*0.25)-1,int(len(x)*0.5)-1,int(len(x)*0.75)-1]) 
-        ax.set_title('$t$ = %03d [ms]' %(t[i]*1000))
-        ax.plot(x[y_IND[0],:], v[y_IND[0],:], c='r', label='75\% ROI')
-        ax.plot(x[y_IND[1],:], v[y_IND[1],:], c='b', label='50\% ROI')
-        ax.plot(x[y_IND[2],:], v[y_IND[2],:], c='g', label='25\% ROI')
-        ax.scatter(x[y_IND[0],:], v[y_IND[0],:], c='r', marker='x', s=(300./fig.dpi)**2)
-        ax.scatter(x[y_IND[1],:], v[y_IND[1],:], c='b', marker='x', s=(300./fig.dpi)**2)
-        ax.scatter(x[y_IND[2],:], v[y_IND[2],:], c='g', marker='x', s=(300./fig.dpi)**2)
-        ax.set_xlim(0, IMG_WIDTH)
-        ax.set_ylim(-200, 200)
-        ax.set_xlabel('$x$[mm]', fontsize = 20)
-        ax.set_ylabel('$v$[mm/s]', fontsize = 20)
+        Y_IND = np.array([int(len(x)*0.25)-1,int(len(x)*0.5)-1,int(len(x)*0.75)-1]) 
+        ax.set_title('$t$ = %03d [ms]' %(t[II]*1000))
+        ax.plot(x_pad[Y_IND[0],:], v_pad[Y_IND[0],:], c='r', label='75\% ROI')
+        ax.plot(x_pad[Y_IND[1],:], v_pad[Y_IND[1],:], c='b', label='50\% ROI')
+        ax.plot(x_pad[Y_IND[2],:], v_pad[Y_IND[2],:], c='g', label='25\% ROI')
+        # ax.scatter(x_pad[Y_IND[0],:], v_pad[Y_IND[0],:], c='r', marker='x', s=(300./fig.dpi)**2)
+        # ax.scatter(x_pad[Y_IND[1],:], v_pad[Y_IND[1],:], c='b', marker='x', s=(300./fig.dpi)**2)
+        # ax.scatter(x_pad[Y_IND[2],:], v_pad[Y_IND[2],:], c='g', marker='x', s=(300./fig.dpi)**2)
+        ax.set_xlim(0, Width)
+        ax.set_ylim(-200, 300)
+        ax.set_xlabel('$x$[mm]')
+        ax.set_ylabel('$v$[mm/s]')
         ax.legend(prop={'size': 12}, ncol = 3, loc='lower center')
         ax.grid(b=True)
-        ax.set_xticks(np.linspace(0, 275, 6))
+        ax.set_xticks(np.linspace(0, Width, 6))
         ax.set_xticklabels(np.linspace(0,5,6, dtype=np.int))
         fig.tight_layout(pad=0.5)
         Name_Out = Fol_Img+os.sep+'profiles%06d.png' %LOAD_IDX
@@ -182,15 +195,15 @@ for i in range(0,N_T):
     if PLOT_FLUX == True:
         fig, ax = plt.subplots(figsize=(9, 5))
         # integrate using trapz
-        q = np.trapz(v, x)
-        ax.set_title('$t$ = %03d [ms]' %(t[i]*1000))
+        q = np.trapz(v_pad, x_pad)/Scale
+        ax.set_title('$t$ = %03d [ms]' %(t[II]*1000))
         ax.scatter(y[:,0], q, c='r', marker='x', s=(300./fig.dpi)**2)
         ax.plot(y[:,0], q, c='r')
-        ax.set_ylim(-30000,40000)
-        ax.set_yticks(np.arange(-30000,40001,10000))
-        ax.set_xlim(0, 1271)
-        ax.set_xticks(np.arange(0,1300,220))
-        ax.set_xticklabels(np.arange(0,21,4))
+        ax.set_ylim(-800,1200)
+        ax.set_yticks(np.linspace(-800,1200,11))
+        ax.set_xlim(0, Height)
+        ax.set_xticks(np.arange(0,Height,4*Scale))
+        ax.set_xticklabels(np.linspace(0,20,6,dtype=int), fontsize=15)
         ax.set_xlabel('$y$[mm]', fontsize = 20)
         ax.set_ylabel('$q$[mm$^2$/s]', fontsize = 20)
         ax.grid(b=True)
@@ -199,14 +212,71 @@ for i in range(0,N_T):
         fig.savefig(Name_Out, dpi=65)
         plt.close(fig)
         IMAGES_FLUX.append(imageio.imread(Name_Out))
-# render the gifs
-imageio.mimsave(GIF_FLUX, IMAGES_FLUX, duration=0.075)
-imageio.mimsave(GIF_PROF, IMAGES_PROF, duration=0.075)
-imageio.mimsave(GIF_QUIV, IMAGES_CONT, duration=0.075)
-imageio.mimsave(GIF_ROI, IMAGES_ROI, duration = 0.075)
-imageio.mimsave(GIF_H, IMAGES_H, duration = 0.075)
+        
+    # plot the contour and the quiver, the principle is the same, so not everything is commented
+    if PLOT_QUIV == True:
+        fig, ax = plt.subplots(figsize = (4, 8))
+        cs = plt.pcolormesh(x_pco,y_pco,v, vmin=-300, vmax=300, cmap = custom_map) # create the contourplot using pcolormesh
+        ax.set_aspect('equal') # set the correct aspect ratio
+        clb = fig.colorbar(cs, pad = 0.2) # get the colorbar
+        clb.set_ticks(np.linspace(-300, 300, 11)) # set the colorbarticks
+        clb.ax.set_title('Velocity \n [mm/s]', pad=15) # set the colorbar title
+        STEPY= 2
+        STEPX = 1
+        plt.quiver(x[(len(x)+1)%2::STEPY, ::STEPX], y[(len(x)+1)%2::STEPY, ::STEPX], u[(len(x)+1)%2::STEPY, ::STEPX],\
+                   v[(len(x)+1)%2::STEPY, ::STEPX], color='k', scale=600, width=0.005,headwidth=4, headaxislength = 6)
+        ax.set_ylim(0,Height)
+        ax.set_xlim(0,Width)
+        ax.set_yticks(np.arange(0,Height,4*Scale)) # set custom y ticks
+        ax.set_yticklabels(np.linspace(0,20,6,dtype=int)) # set custom y ticklabels
+        ax.set_xticks(np.linspace(0, Width, 6)) # set custom x ticks 
+        ax.set_xticklabels(np.arange(0,6,1)) # set custom x ticklabels
+        ax.set_xlabel('$x$[mm]')
+        ax.set_ylabel('$y$[mm]')
+        fig.tight_layout(pad=0.5)
+        Name_Out = Fol_Img+os.sep+'contour%06d.png'%LOAD_IDX
+        fig.savefig(Name_Out, dpi=65)
+        plt.close(fig)
+        IMAGES_CONT.append(imageio.imread(Name_Out))
+        
+    if PLOT_HIGHPASS == True:
+        fig, ax = plt.subplots(figsize = (4, 8))
+        ax.imshow(img, cmap=plt.cm.gray) # show the image
+        STEPY= 1
+        STEPX = 1
+        plt.quiver(x[(len(x)+1)%2::STEPY, ::STEPX], y[(len(x)+1)%2::STEPY, ::STEPX], u_hp[(len(x)+1)%2::STEPY, ::STEPX],\
+                   v_hp[(len(x)+1)%2::STEPY, ::STEPX], color='lime', scale=100, width=0.005,headwidth=4, headaxislength = 6)
+        ax.set_ylim(0,Height)
+        ax.set_xlim(0,Width)
+        ax.set_yticks(np.arange(0,Height,4*Scale)) # set custom y ticks
+        ax.set_yticklabels(np.linspace(0,20,6,dtype=int)) # set custom y ticklabels
+        ax.set_xticks(np.linspace(0, Width, 6)) # set custom x ticks 
+        ax.set_xticklabels(np.arange(0,6,1)) # set custom x ticklabels
+        ax.set_xlabel('$x$[mm]')
+        ax.set_ylabel('$y$[mm]')
+        fig.tight_layout(pad=0.5)
+        Name_Out = Fol_Img+os.sep+'highpass%06d.png'%LOAD_IDX
+        fig.savefig(Name_Out, dpi=65)
+        plt.close(fig)
+        IMAGES_HIGHPASS.append(imageio.imread(Name_Out))
+# # render the gifs
+duration = 0.05
+if PLOT_FLUX == True:
+    imageio.mimsave(GIF_FLUX, IMAGES_FLUX, duration=duration)
+if PLOT_PROF == True:
+    imageio.mimsave(GIF_PROF, IMAGES_PROF, duration=duration)
+if PLOT_QUIV == True:
+    imageio.mimsave(GIF_QUIV, IMAGES_CONT, duration=duration)
+if PLOT_ROI == True:
+    imageio.mimsave(GIF_ROI, IMAGES_ROI, duration=duration)
+if PLOT_HT == True:
+    imageio.mimsave(GIF_H, IMAGES_H, duration=duration)
+if PLOT_HIGHPASS == True:
+    imageio.mimsave(GIF_HIGHPASS, IMAGES_HIGHPASS, duration=duration)
+if PLOT_HIST == True:
+    imageio.mimsave(GIF_HIST, IMAGES_HIST, duration=duration)
 # delete the folder with the gif images
-shutil.rmtree(Fol_Img)
+# shutil.rmtree(Fol_Img)
 
 
 

eb5f05beee6988d880db9da8f668241b395673e2
ManuelRatz <70625199+ManuelRatz@users.noreply.github.com>
2020-12-14 16:48:32 +0100

    Added velocity tensor smoothing

--- PIV_Campaign_Processing/post_processing_functions.py
+++ PIV_Campaign_Processing/post_processing_functions.py
@@ -9,7 +9,7 @@ import numpy as np              # for array operations
 import os                       # for file paths
 import cv2                      # for image reading
 import scipy.signal as sci 
-import smoothn
+from smoothn import smoothn
 from scipy.ndimage import gaussian_filter
 
 def pad(x, y, u, v, width):
@@ -80,7 +80,7 @@ def calc_flux(x, v, Scale):
     # return the result
     return q
 
-def shift_grid(x, y):
+def shift_grid(x, y, padded = True):
     """
     Function to shift the grid by half the window size for pcolormesh
 
@@ -107,6 +107,9 @@ def shift_grid(x, y):
         Array containing the padded v component for every interrogation window.
 
     """
+    if padded == True:
+        x = x[:,1:-1]
+        y = y[:,1:-1]
     # calculate half the width and height of the windows
     half_width = (x[0,1] - x[0,0])/2
     half_height = (y[0,0] - y[1,0])/2
@@ -586,19 +589,329 @@ def high_pass(u, v, sigma, truncate):
     # return the result
     return u_filt, v_filt
 
-def fill_dummy(u, v, NY_max):
+def fill_dummy(u, v, NY_max, Fill_dummy):
     missing_rows = NY_max - u.shape[0]
     dummy_zeros = np.ones((missing_rows, u.shape[1]))*1000
     u = np.vstack((dummy_zeros, u))
     v = np.vstack((dummy_zeros, v))
     return u, v
 
+def fill_nan(x, y, NY_max):
+    missing_rows = NY_max - x.shape[0]
+    dummy_zeros = np.ones((missing_rows, x.shape[1]))*np.nan
+    x = np.vstack((dummy_zeros, x))
+    y = np.vstack((dummy_zeros, y))
+    return x, y
 
-# Fol_In = 'C:\PIV_Processed\Images_Processed\Rise_64_16_peak2RMS\Results_R_h1_f1200_1_p15_64_16'
-# NX = get_column_amount(Fol_In)
-# NY_max = get_max_row(Fol_In, NX)
-# # Fol_Raw = get_raw_folder(Fol_In)
-# # img = load_raw_image(Fol_Raw, 50)
+def load_and_smooth(Fol_Sol, order = 15):
+    def smooth_frequency(profiles):
+        """
+        Function to smooth the velocity profiles along the time axis in the frequency
+        domain. For this we are using filtfilt and firwin from scipy. The principle
+        as follows:
+            1) For each row and column in the velocity field we extract the row in
+               time (henceforth called timerow).
+            2) Because not all timerows have data in them (some have 100s) we find
+               the first index not equal to 100, this is our starting index. We then
+               search for the second index not equal to 100. If there is none, all
+               other points are valid, so we take all of those points after the first
+            3) If there is a second one we check the number of steps in between.
+               If it is below 50, we search again after the second peak as the array
+               is either too short (then nothing happens) or we get a longer section
+               that is valid for our smoothing
+            4) We then smooth the extracted profile in time using a Firwin length
+               of 1/30th of the profile length. The smoothed array is then returned
+    
+        Parameters
+        ----------
+        profiles : 3d np.array
+            3d Tensor containing one velocity component of the field of every timestep.
+            The axiies are as follows:
+                0 - time
+                1 - y (vertical)
+                2 - x (horizontal)
+    
+        Returns
+        -------
+        smoothed_array : 3d np.array
+            3d Tensor containing one smoothed velocity component of the field of 
+            every timestep.
+    
+        """
+        def smooth_profile(tmp_prof):
+            """
+            Function to smooth a 1d profile in time using firwin and filtfilt.
+            IMPORTANT: The profile must not contain nans otherwise filtfilt will
+            only amplify this problem
+    
+            Parameters
+            ----------
+            tmp_prof : 1d np.array
+                Array containing one velocity component at one position in time.
+    
+            Returns
+            -------
+            prof_filtered : 1d np.array
+                Array containing one smoothed velocity component at one position in time.
+    
+            """
+            # select the windows
+            filter_poly = sci.firwin(N_T//30, 0.1 , window='hamming', fs = 100)
+            # filter with filtfilt
+            prof_filtered = sci.filtfilt(b = filter_poly, a = [1], x = tmp_prof, padlen = 10, padtype = 'odd')
+            # return the filtered profile
+            return prof_filtered
+        def extract_profile(time_column):
+            """
+            Function to extract the longest velocity profile in time from the
+            current time step.
+    
+            Parameters
+            ----------
+            time_column : 1 np.array of length N_T (total number of images)
+                DESCRIPTION.
+    
+            Returns
+            -------
+            tmp_prof : 1d np.array
+                Temporary array containing a velocity profile in time at a fixed
+                point in space.
+            id_min : int
+                Lower index at which the array is sliced.
+            id_max : int
+                Upper index at which the array is sliced.
+    
+            """
+            # get the first index at which we dont have 100
+            id1 = np.argmax(time_column < 0.8*Fill_Dum)
+            # get the next index after id1 at which we have 100
+            id2 = np.argmax(time_column[id1:] > 0.8*Fill_Dum)
+            # check if the timeprofile is long enough
+            if id2 < 50:
+                # attempt to find a second non 100 index somewhere else
+                id2_second_attempt = np.argmax(time_column[id1+id2+2:] > 0.8*Fill_Dum)
+                # np.argmax returns 0 if nothing was found, so test this here
+                if id2_second_attempt == 0:
+                    # if true we set the second index to the last of the time_column
+                    id2_second_attempt = time_column.shape[0]-1
+                # set minimum and maximum index accordingly
+                id_min = id1 + id2+2
+                id_max = id2_second_attempt
+            else:
+                # set minimum and maximum index accordingly
+                id_min = id1
+                id_max = id1 + id2
+            # slice the time profile
+            tmp_prof = time_column[id_min:id_max]
+            # return the sliced array and the two indices
+            return tmp_prof, id_min, id_max
+        
+        # start by copying the array to avoid editing the original array
+        smoothed_array = np.copy(profiles)
+        # iterate across the y axis
+        for j in range(0, smoothed_array.shape[1]):
+            # iterate across the x axis
+            for i in range(0, smoothed_array.shape[2]):
+                # extract the longest profile in time
+                tmp_prof, id1, id2 = extract_profile(smoothed_array[:,j,i])
+                # check if it is long enough for filtfilt
+                if tmp_prof.shape[0] < 100:
+                    continue
+                # smooth the respective profile in time and copy into the dummy array
+                smoothed_array[id1:id2,j,i] = smooth_profile(tmp_prof)
+        # return the smoothed array
+        return smoothed_array
+
+    def smooth_vertical(profiles):
+        """
+        Function to smooth the 3d tensor along the vertical axis
+    
+        Parameters
+        ----------
+        profiles : 3d np.array
+            3d Tensor containing one velocity component of the field of 
+            every timestep. The invalid positions are filled with 1000s.
+    
+        Returns
+        -------
+        work_array_smoothed : 3d np.array
+            3d Tensor containing smoothed one velocity component of the field of 
+            every timestep. The invalid positions are filled with 1000s. 
+    
+        """
+        # create a dummy to fill
+        smoothed_array = np.ones(profiles.shape)*Fill_Dum
+        # iterate over each time step
+        for j in range(0, smoothed_array.shape[0]):
+            # find the first index at which we dont have 100
+            valid_idx = np.argmax(np.abs(profiles[j,:,1]) < 0.8*Fill_Dum)
+            # smooth along the vertical axis sliced after the valid idx
+            smoothed_array[j,valid_idx:,:], Dum, Dum, Dum = smoothn(profiles[j,valid_idx:,:], s = 0.7, axis = 0, isrobust = True)
+        return smoothed_array
+    
+    def smoothn_horizontal(profiles):
+        """
+        Function to smooth the profiles along the horizontal axis
+    
+        Parameters
+        ----------
+        profiles : 3d np.array
+            3d Tensor containing one velocity component of the field of 
+            every timestep. The invalid positions are filled with 1000s.
+    
+        Returns
+        -------
+        smoothed_array : 3d np.array
+            3d Tensor containing one smoothed velocity component of the field of 
+            every timestep. The invalid positions are filled with 1000s.
+    
+        """
+        # create a dummy to fill
+        smoothed_array = np.ones(profiles.shape)*Fill_Dum
+        # iterate in time
+        for i in range(0, profiles.shape[0]):
+            # iterate along y axis
+            for j in range(0, profiles.shape[1]):
+                # get the profile along x
+                prof_hor = profiles[i,j,:]
+                # check that we do not have one containing 100s
+                if np.mean(prof_hor) > 0.8*Fill_Dum:
+                    continue
+                else:
+                    # flip the signal
+                    dum_flip = np.flip(prof_hor)
+                    # pad the profile by extending it twice and flipping these extensions
+                    padded_profile = np.hstack((-dum_flip[:-1], prof_hor, -dum_flip[1:]))
+                    # smooth the new profile
+                    smoothed_pad, DUMMY, DUMMY, DUMMY = smoothn(padded_profile, s = 0.125, isrobust = True)
+                # copy the sliced, smoothed profile into the tensor
+                smoothed_array[i,j,:] = smoothed_pad[v.shape[1]-1:2*v.shape[1]-1]
+        return smoothed_array      
+    
+    def smooth_horizontal_sin(profiles, x_local, Width):
+        """
+        Function to smooth the horizontal profiles using a sin transformation.
+        For that we are computing a temporal base Psi of sin(x*n*pi/Width), n = 1,2,3,...
+        from which we calculate a projection matrix via Psi*Psi.T.
+        Multiplying this new matrix with the horizontal profiles gives a smoothed 
+        version that satisfies the boundary conditions automatically.
+        
+        This makes the 'smoothn_horizontal' function obsolete
+    
+        Parameters
+        ----------
+        profiles : 3d np.array
+            3d Tensor containing one velocity component of the field of 
+            every timestep. The invalid positions are filled with 1000s.
+        x_local : 1d np.array
+            1d array containing the x coordinates of the velocity field in pixels.
+        Width : int
+            Width of the channel in pixels.
+    
+        Returns
+        -------
+        smoothed_array : 3d np.array
+            3d Tensor containing one smoothed velocity component of the field of 
+            every timestep.
+    
+        """
+        # define the n'th base function
+        def basefunc(x, n, width, norm):                                                                                    
+            return np.sin(n*x*np.pi/width)/norm  
+        # initialize Psi, we remove 5 degrees of freedom with this
+        Psi = np.zeros((x_local.shape[0],order))
+        # calculate the norm
+        norm = np.sqrt(x_local[-1]/2)
+        # fill the Psi columns with the sin profiles
+        for i in range(1, Psi.shape[1]+1):
+            Psi[:,i-1] = basefunc(x_local, i, x_local[-1], norm)
+        # calculate the projection
+        projection = np.matmul(Psi,Psi.T)
+        # create a dummy to fill
+        smoothed_array = np.ones(profiles.shape)*Fill_Dum
+        # iterate in time
+        for i in range(0, profiles.shape[0]):
+            # iterate along y axis
+            for j in range(0, profiles.shape[1]):
+                # get the profile along x
+                prof_hor = profiles[i,j,:]
+                # check that we do not have one containing 100s
+                if np.mean(prof_hor) > 0.8*Fill_Dum:
+                    continue
+                else:
+                    smoothed_array[i,j,:] = np.matmul(projection, prof_hor)*8
+                    # the multiplication by 8 is required for some reason to get
+                    # the correct magnitude, at the moment the reason for this is
+                    # unclear. For a Fall this has to be 12 for some reason.
+        return smoothed_array
+    # extract the constants from the images
+    # give the first index and the number of files
+    Frame0 = get_frame0(Fol_Sol)
+    N_T = get_NT(Fol_Sol)
+    NX = get_column_amount(Fol_Sol) # number of columns
+    Fol_Raw = get_raw_folder(Fol_Sol)
+    NY_max = get_max_row(Fol_Sol, NX) # maximum number of rows
+    Height, Width = get_img_shape(Fol_Raw) # image width in pixels
+    Scale = Width/5 # scaling factor in px/mm
+    Dt = 1/get_frequency(Fol_Raw) # time between images
+    Factor = 1/(Scale*Dt) # conversion factor to go from px/frame to mm/s
+    # this is a fill dummy, meaning all invalid positions are filled with 1000
+    Fill_Dum = 1000
+    print('Loading Velocity Fields')
+    """
+    Load the velocity profiles and put them into the 3d tensor in which all profiles are stored.
+    
+    For that we load the velocity field for each time step and add 0 padding at the edges.
+    Afterwards we fill the velocity field to the top with 100s because not every index
+    has the full region of interest, so in order to be able to copy it into the 3d tensor
+    we have to add dummys at the top. In case something messes up, we use 100 and not 
+    np.nan because firwin cannot deal with nans
+    """
+    # initialize the velocity profile tensors, the width is +2 because we have 0 padding
+    profiles_u = np.zeros((N_T, NY_max, NX+2))
+    profiles_v = np.zeros((N_T, NY_max, NX+2))
+    tensor_x = np.zeros((N_T, NY_max, NX+2))
+    tensor_y = np.zeros((N_T, NY_max, NX+2))
+    
+    for i in range(0, N_T):
+        Load_Index = Frame0 + i # load index of the current file
+        x, y, u, v, ratio, mask = load_txt(Fol_Sol, Load_Index, NX) # load the data from the txt
+        x, y, u, v, ratio, valid, invalid = filter_invalid(x, y, u, v, ratio, mask, valid_thresh = 0.5) # filter invalid rows
+        x, y, u, v = pad(x, y, u, v, Width) # 0 pad at the edges
+        u = u*Factor
+        v = v*Factor
+        u, v = fill_dummy(u, v, NY_max, Fill_Dum) # fill the 2d velocity array with 1000s
+        x, y = fill_nan(x, y, NY_max) # fill the 2d velocity array with 1000s
+        profiles_v[i,:,:] = v # store in the tensor
+        profiles_u[i,:,:] = u # store in the tensor
+        tensor_x[i,:,:] = x # store in the tensor
+        tensor_y[i,:,:] = y # store in the tensor
+        
+    """
+    We now smooth along all three axiies, first timewise (axis 0) in the frequency
+    domain, then along the vertical direction (axis 1) with robust smoothn and finally
+    along the horizontal axis, again with smoothn.
+    """
+    print('Smoothing along the time axis')
+    smoothed_freq = smooth_frequency(profiles_v)
+    
+    print('Smoothing along the vertical axis')
+    smoothed_vert_freq = smooth_vertical(smoothed_freq)
+    
+    print('Smoothing along the horizontal axis with smoothn')
+    smoothed_tot = smoothn_horizontal(smoothed_vert_freq)
+    # filter the values filled with the dummy
+    inv = smoothed_tot > 0.8*Fill_Dum
+    smoothed_tot[inv] = np.nan    
+    
+    print('Smoothing along the horizontal axis with cosine transformation')
+    smoothed_tot_cos = smooth_horizontal_sin(smoothed_vert_freq, x[0,:], Width)
+    # filter the values filled with the dummy
+    inv = smoothed_tot_cos > 0.8*Fill_Dum
+    smoothed_tot_cos[inv] = np.nan
+
+    return tensor_x, tensor_y, profiles_u, profiles_v, smoothed_tot_cos, smoothed_tot
+
 
 def custom_div_cmap(numcolors=11, name='custom_div_cmap',
                     mincol='blue', midcol='white', maxcol='red'):
@@ -617,8 +930,8 @@ def custom_div_cmap(numcolors=11, name='custom_div_cmap',
 
 
 def calc_qfield(x, y, u, v):
-    u_smo, dum, dum, dum = smoothn.smoothn(u, s = 5)
-    v_smo, dum, dum, dum = smoothn.smoothn(v, s = 5)
+    u_smo, dum, dum, dum = smoothn(u, s = 5)
+    v_smo, dum, dum, dum = smoothn(v, s = 5)
     # calculate the derivatives
     ux = np.gradient(u_smo, x[0,:], axis = 1)
     uy = np.gradient(u_smo, y[:,0], axis = 0)
@@ -626,6 +939,7 @@ def calc_qfield(x, y, u, v):
     vy = np.gradient(v_smo, y[:,0], axis = 0)
     qfield = -0.5*(ux**2+2*vx*uy+vy**2)
     return qfield
+
 
 def get_frame0(Fol_In):
     Fol = os.path.join(Fol_In, 'data_files')
@@ -638,7 +952,6 @@ def get_NT(Fol_In):
     N_T = len(os.listdir(os.path.join(Fol_In, 'data_files')))
     return N_T
 
-Fill_Dum = 1000
 
 # Fol_In = 'C:\PIV_Processed\Images_Processed\Fall_24_24_peak2RMS\Results_F_h4_f1200_1_s_24_24'
 # Fol_Raw = get_raw_folder(Fol_In)

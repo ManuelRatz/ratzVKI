ba3dd67f11e30e35abda00fe1dddc4f0ad5fefc8
ManuelRatz <70625199+ManuelRatz@users.noreply.github.com>
2020-12-15 15:40:51 +0100

    finalizing animation and smoothing testing

--- PIV_Campaign_Processing/Post_Processing/smooth_rise_test.py
+++ PIV_Campaign_Processing/Post_Processing/smooth_rise_test.py
@@ -13,6 +13,24 @@ import post_processing_functions as ppf
 import matplotlib.pyplot as plt
 
 
+
+
+# load the data set and get the parameters of the images and the txt files
+ppf.set_plot_parameters(20, 15, 10)
+Fol_Sol = 'C:\PIV_Processed\Images_Processed\Rise_64_16_peak2RMS\Results_R_h2_f1200_1_p13_64_16'
+
+# for i in range(0, 10):
+order = 18
+x_tensor, y_tensor, u_tensor, v_tensor_raw, v_tensor_smoothed_smoothn = ppf.load_and_smooth(Fol_Sol, order = order)
+idx = 25
+fig, ax = plt.subplots()
+# ax.plot(x_tensor[idx,-1,:], v_tensor_smoothed_sin[idx,-1,:])
+ax.plot(x_tensor[idx,-1,:], v_tensor_raw[idx,-1,:])
+ax.plot(x_tensor[idx,-1,:], v_tensor_smoothed_smoothn[idx,-1,:])
+
+
+#%%
+import post_processing_functions as ppf
 def flux_parameters(smoothed_profile, x, Scale):
     """
     Function to get the minimum and maximum flux for the mass flux
@@ -54,7 +72,7 @@ def flux_parameters(smoothed_profile, x, Scale):
     q_ticks = np.linspace(q_min, q_max, divider_max - divider_min+1)
     return  q_max, q_min, q_ticks
 
-def profile_parameters(smoothed_profile):
+def quiver_parameters(smoothed_profile):
     # increments of the vertical axis
     increments = 50
     maximum_v = np.nanmax(smoothed_profile)
@@ -64,74 +82,78 @@ def profile_parameters(smoothed_profile):
     divider_min = int(np.floor(minimum_v/increments))
     v_min = divider_min*increments
     v_ticks = np.linspace(v_min, v_max, divider_max - divider_min+1)
-    if v_min - minimum_v > -50:
-        v_min = v_min - 50
     return v_max, v_min, v_ticks
 
+qfield = ppf.calc_qfield(x_tensor, y_tensor, u_tensor, v_tensor_smoothed_smoothn)
+#%%
 
-# load the data set and get the parameters of the images and the txt files
-ppf.set_plot_parameters(20, 15, 10)
-Fol_Sol = 'C:\PIV_Processed\Images_Processed\Rise_64_16_peak2RMS\Results_R_h2_f1200_1_p13_64_16'
+# def qfield_parameters(qfield):
+#     qfield_sorted = np.sort(qfield.ravel())
+    
+#     return Maximum_q, Minimum_q
+# params = qfield_parameters(qfield)
 
-# for i in range(0, 10):
-order = 18
-x_tensor, y_tensor, u_tensor, v_tensor_raw, v_tensor_smoothed_sin, v_tensor_smoothed_smoothn = ppf.load_and_smooth(Fol_Sol, order = order)
-idx = 8
+k = np.sort(qfield.ravel())
+valid = np.isfinite(k)
+k = k[valid]
+k = k[int(500):k.shape[0]-500]
+
 fig, ax = plt.subplots()
-ax.plot(x_tensor[idx,-1,:], v_tensor_smoothed_sin[idx,-1,:])
-ax.plot(x_tensor[idx,-1,:], v_tensor_raw[idx,-1,:])
-ax.plot(x_tensor[idx,-1,:], v_tensor_smoothed_smoothn[idx,-1,:])
-
+ax.hist(k, bins = 150, range = (k[0],k[-1]))
+ax.set_ylim(0, 100)
 
 #%%
-t = 100
-x = x_tensor[t,:,:]
-y = y_tensor[t,:,:]
-u = u_tensor[t,:,:]
-v = v_tensor_smoothed_smoothn[t,:,:]
-qfield = ppf.calc_qfield(x, y, u, v)
-x_pco, y_pco = ppf.shift_grid(x, y)
+def slice_nans(array):
+    nans = np.argwhere(np.isnan(array[:,5]))
+    return nans.shape[0]
+Height, Width = ppf.get_img_shape(ppf.get_raw_folder(Fol_Sol))
 
-fig, ax = plt.subplots(figsize = (4.5, 8))
-cs = plt.pcolormesh(x,y,qfield, cmap = plt.cm.Blues, alpha = 0.7, visible = True, linewidth = 0.000005) # create the contourplot using pcolormesh
-ax.set_aspect('equal') # set the correct aspect ratio
-clb = fig.colorbar(cs, pad = 0.2) # get the colorbar
-# clb.set_ticks(np.linspace(-100, 0, 6)) # set the colorbarticks
-clb.ax.set_title('Q Field \n [1/s$^2$]', pad=15) # set the colorbar title
+import imageio
+import os
+IMAGES = []
+Fol_Img = ppf.create_folder('tmp')
+for i in range(0, 300):
+    idx = 50 + i*4
+    print(i)
+    valid_row = slice_nans(u_tensor[idx,:,:])
+    x_pad = x_tensor[idx,valid_row:,:]
+    y_pad = y_tensor[idx,valid_row:,:]
+    u_pad = u_tensor[idx,valid_row:,:]
+    v_pad = v_tensor_raw[idx,valid_row:,:]
+    x = x_pad[:,1:-1]
+    y = y_pad[:,1:-1]
+    u = u_pad[:,1:-1]
+    v = v_pad[:,1:-1]
+    qfield = ppf.calc_qfield(x, y, u, v)
+    x_pco, y_pco = ppf.shift_grid(x, y, padded = False)
+    fig, ax = plt.subplots(figsize = (4.5, 8))
+    cs = plt.pcolormesh(x_pco, y_pco, qfield, cmap = plt.cm.viridis, alpha = 1, vmin = -0.03, vmax = 0.02) # create the contourplot using pcolormesh
+    ax.set_aspect('equal') # set the correct aspect ratio
+    clb = fig.colorbar(cs, pad = 0.2, drawedges = False, alpha = 1) # get the colorbar
+    # clb.set_ticks(np.linspace(-100, 0, 6)) # set the colorbarticks
+    clb.ax.set_title('Q Field \n [1/s$^2$]', pad=15) # set the colorbar title
+    ax.set_xlim(0,Width)
+    ax.set_ylim(0,Height)
+    u_hp, v_hp = ppf.high_pass(u, v, 3, 3)
+    StpX = 1
+    StpY = 1
+    
+    headlength = 5
+    headwidth = 0.5*headlength
+    width = 0.005
+    ax.grid(b = False)
+    ax.quiver(x[(x.shape[0]+1)%2::StpY,::StpX], y[(x.shape[0]+1)%2::StpY,::StpX],\
+              u_hp[(x.shape[0]+1)%2::StpY,::StpX], v_hp[(x.shape[0]+1)%2::StpY,::StpX],\
+              scale = 300, color = 'k', scale_units = 'height', units = 'width', width = width,\
+              headlength = headlength, headwidth = headwidth, headaxislength = headlength)
+    Name_Out = Fol_Img + os.sep + 'test%06d.png' %idx
+    fig.savefig(Name_Out, dpi = 80)
+    plt.close(fig)
+    IMAGES.append(imageio.imread(Name_Out))
 
-u, v = ppf.high_pass(u, v, 3, 3)
-StpX = 1
-StpY = 1
-ax.grid(b = False)
-ax.quiver(x[::StpY,::StpX], y[::StpY,::StpX], u[::StpY,::StpX], v[::StpY,::StpX], scale = 70, color = 'red')
-fig.savefig('test.png', dpi = 80)
+imageio.mimsave('test.gif', IMAGES, duration = 0.075)
+import shutil
+shutil.rmtree(Fol_Img)
 
-# animate a quick velocity profile comparison to see raw vs smoothed
-# ppf.set_plot_parameters(20, 15, 10)
-# t = 100 # start after 50 timesteps
-# y = -2 # second row from the bottom
-# import imageio
-# import os
-# fol = ppf.create_folder('tmp')
-# Gifname = 'Smoothing_comparison.gif'
-# listi = []
-# N_T = 100
-# for i in range(0, N_T):
-#     print('Image %d of %d' %((i+1),N_T))
-#     fig, ax = plt.subplots(figsize = (8,5))
-#     ax.plot(x[0,:], profiles_v[t+6*i,y,:], label = 'Unsmoothed', c = 'k')
-#     ax.plot(x[0,:], smo_tot[t+6*i,y,:], label = 'Smoothn', c = 'lime')
-#     ax.plot(x[0,:], smo_tot_cos[t+6*i,y,:], label = 'Sin Transformation', c = 'r')
-#     ax.legend(loc = 'lower right')
-#     ax.set_xlim(0, Width)
-#     ax.set_ylim(-200, 400)
-#     ax.grid(b = True)
-#     ax.set_xlabel('$x$[mm]')
-#     ax.set_ylabel('$v$[px/frame]')
-#     Name = fol + os.sep +'comp%06d.png' %i
-#     fig.savefig(Name, dpi = 65)
-#     listi.append(imageio.imread(Name))
-#     plt.close(fig)
-# imageio.mimsave(Gifname, listi, duration = 0.05)
-# import shutil
-# shutil.rmtree(fol)
+#%%
+
